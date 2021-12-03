b141cfecfbc50c6d36501ea01fd3904705050843
“VenuReddy773” <venu706@gmail.com>
2021-12-03 21:30:38 +0530

    [VenuGopalReddy] Added: Reflection Demo

---
+++ Reflection/Reflection/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Reflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Customer check = new Customer();
+            check.PrintName();
+        }
+    }
+}
